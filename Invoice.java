674cec92be9084900bf91d4a790c2e844d313280
nazwasalsabila <132126026+nazwasalsabila@users.noreply.github.com>
2023-11-28 12:47:50 +0700

    Update Invoice.java

--- Invoice.java
+++ Invoice.java
@@ -1,7 +1,42 @@
 /**
  * Invoice
  */
-class Invoice {
+public class Invoice {
+    public Transaksi transaksi;
+    public Pembayaran pembayaran;
 
+    abstract class Pembayaran {
+        public String id;
     
+        public Pembayaran(String id) {
+            this.id = id;
+        }
+    
+    }
+
+    class QRIS extends Pembayaran {
+
+        public QRIS(String id) {
+            super(id);
+    
+        }
+    
+    }
+    
+    class Bank extends Pembayaran {
+    
+        public Bank(String id) {
+            super(id);
+        }
+    
+    }
+    
+    class COD extends Pembayaran {
+    
+        public COD(String id) {
+            super(id);
+        }
+    
+    }
+
 }
